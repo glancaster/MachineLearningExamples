3d28a99a945c86cbc8bf3f4e81136a550317e8fa
glancaster <sailthesouth@gmail.com>
2024-07-28 11:47:22 -0400

    revisit OOD

--- ObjectOrientedDesign/BuildingSkillsInObjectOrientedDesignV4/oo-design/src/hw.py
+++ ObjectOrientedDesign/BuildingSkillsInObjectOrientedDesignV4/oo-design/src/hw.py
@@ -1,1 +1,30 @@
-print("hello world") 
+"""
+A Hello World to be sure all our tools work
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Greeting:
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+
+    greeting: str
+    audience: str
+
+    def __str__(self) -> str:
+        return f"{self.greeting} {self.audience}"
+
+
+def main() -> None:
+    """main"""
+    g = Greeting("hello", "world")
+    print(g)
+
+
+if __name__ == "__main__":
+    main()
